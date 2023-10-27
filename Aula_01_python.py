147fb643b4cbbe4e1b34a99dec0f6f643c698524
kayh21 <145858751+kayh21@users.noreply.github.com>
2023-10-27 13:59:10 -0300

    Add files via upload

---
+++ Aula_01_python.py
@@ -0,0 +1,30 @@
+# Algoritmo é uma sequência de ações utilizadas para resolução 
+# Fluxograma: site para criação https://www.lucidchart.com/pages/pt
+# Pseudocódigo: é escrito de forma clara e legível, exemplo Portugol site https://univali-lite.github.io/Portugol-Studio/
+# Descrição narrativa: utiliza o word
+# Linguagem de programação: utiliza editores de código como o VSCODE, Collab e Replit
+# Utiliza-se para criar sprintsUma sprint é uma reunião de pessoas envolvidas num projeto para promover um desenvolvimento mais focalizado do projeto.
+# o termo está fortemente relacionado ao framework de desenvolvimento ágil Scrum. https://miro.com/pt/
+
+# Criar conta no Github ou GitLab para armazenar os códigos desenvolvidos 
+# REDME é um repositório: Um repositório de software é um local de armazenamento de onde pacotes de software podem ser recuperados e instalados em um computador.
+# Marckdown é usado para formatar arquivos README: https://markdownlivepreview.com/
+# A linguagem de programação é um método padronizado, formado por um conjunto de regras sintáticas e semânticas, de implementação de um código fonte - que pode ser compilado e 
+# transformado em um programa de computador, ou usado como script 
+# interpretado - que informará instruções de processamento ao computador.
+
+# PORQUE  Python?
+# - Projetado para ser intuitivo e fácil de programar (sem sacrificar o poder)
+# - Código aberto, com uma grande comunidade de pacotes e recursos
+# - Uma das linguagens de programação mais usadas no mundo
+# - Linguagem “Testado e Verdadeiro” que está em desenvolvimento há décadas
+# - Visualizações de alta qualidade
+# - Funciona na maioria dos sistemas operacionais e plataformas
+
+
+# O que são Variáveis?
+# Variáveis são símbolos usados na programação para armazenar e representar valores, permitindo manipulação e cálculos em um programa. 
+# Cada variável tem um nome único e um tipo que define o tipo de valor que pode ser armazenado nela.
+# Variáveis Numéricas: INT (inteiros)
+# Variáveis de Caracter: STR (Strings) são textos e/ou caracter
+# Variáveis Números reais: Float 
